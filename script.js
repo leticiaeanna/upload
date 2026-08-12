8f1fbd9635c0d3e247918ed9e888e8cb11c6e262
leticia mendes <leticiaeanna678@gmail.com>
2026-08-12 11:53:21 -0300

    configurações

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const inputUpload = document.getElementById('meu-upload')
+const inputUpload = document.getElementById('avatar')
 
-inputUpload.addEventListener('change',function(evento) {
- const arquivo = evento.target.files[0]
+inputUpload.addEventListener('change', function (evento) {
+   const arquivo = evento.target.files[0]
 
-if (arquivo) {
- const nomeDaImagem = arquivo.name 
-console.log(nomeDaImagem)
-} else {
-   console.log("Nenhum arquivo selecionado");
-}
+   if (arquivo) {
+      const nomeDaImagem = arquivo.name
+      console.log(nomeDaImagem)
+   } else {
+      console.log("Nenhum arquivo selecionado");
+   }
 })
+
+const nome = document.getElementById("nome")
+
+const b = document.getElementById("btn")
+
+b.addEventListener("click", () => {
+   console.log(">>>>>>>")
+})
+________________________________________________________________________________________________________
